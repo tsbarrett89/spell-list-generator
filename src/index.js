12d5c8e5307a6db6485e90328a79e0891c3a89fe
tsbarrett89 <tsbarrett89@gmail.com>
2022-05-04 17:06:20 -0600

    imported neccessary dependencies

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { configureStore } from 'redux';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import './index.css';
+
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));

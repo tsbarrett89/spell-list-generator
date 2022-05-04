c93132acf73ec733d5a9cb5d56062342bb4107f6
tsbarrett89 <tsbarrett89@gmail.com>
2022-05-04 17:18:32 -0600

    added thunk middleware

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { configureStore } from 'redux';
+import { applyMiddleware, createStore, compose } from 'redux';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { ThunkMiddleware as Thunk } from 'redux-thunk';
 
 import './index.css';
 
 import App from './App';
 import reducer from './reducer';
 
-const store = configureStore(reducer)
+const store = createStore(reducer, applyMiddleware(Thunk))
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(

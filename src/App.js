357326d137aa6a14a2a39cf89a3d3115a1bb4e74
tsbarrett89 <tsbarrett89@gmail.com>
2022-05-04 17:28:37 -0600

    started routing in app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     
+      <Route exact path='/' />
     </div>
   );
 }

40952dd2add02ecbabaa9608f4bcd1b67545a5a4
tsbarrett89 <tsbarrett89@gmail.com>
2022-05-05 12:23:36 -0600

    form skeleton added

--- src/components/users/userLogin.js
+++ src/components/users/userLogin.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
 
-const userLogin = (props) => {
+const UserLogin = () => {
+    const { register, handleSubmit } = useForm()
 
+    const onSubmit = data => console.log(data)
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <input {...register("email")} />
+            <input {...register("password")} />
+            <button type="submit" />
+        </form>
+    )
 }
 
-export default userLogin
+export default UserLogin

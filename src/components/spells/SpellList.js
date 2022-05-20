e24bf93f04a8d30580d1e11f0c3f45836a6dbddf
tsbarrett89 <tsbarrett89@gmail.com>
2022-05-20 15:08:01 -0600

    fixed import issue on fetchSpells

--- src/components/spells/SpellList.js
+++ src/components/spells/SpellList.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
-import fetchSpells from '../../actions';
+import { fetchSpells } from '../../actions';
 
 const SpellList = () => {
     const spellList = useSelector((state) => state.spells)

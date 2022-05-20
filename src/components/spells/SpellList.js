06179f7fb35b924103d52a7eb965456ba030be8c
tsbarrett89 <tsbarrett89@gmail.com>
2022-05-20 15:01:51 -0600

    useEffect imported, dispatch assigned, useEffect created

--- src/components/spells/SpellList.js
+++ src/components/spells/SpellList.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
 const SpellList = () => {
     const spellList = useSelector((state) => state.spells)
     const isFetching = useSelector((state) => state.isFetching)
+    const dispatch = useDispatch()
 
-
+    useEffect(() => {
+        
+    }, [])
 
     return (
         <div>

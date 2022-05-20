0b102bcc4e38cf47ffec7a6d0ee13dbb6ed366e3
tsbarrett89 <tsbarrett89@gmail.com>
2022-05-20 14:54:17 -0600

    SpellList imported and set to path=/

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 
 import UserLogin from './components/users/UserLogin';
 import UserRegistration from './components/users/UserRegistration';
+import SpellList from './components/spells/SpellList';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <h1>Does this work?</h1>
       
       <Routes>
+        <Route path='/' element={<SpellList />} />
         <Route path='/login' element={<UserLogin />} />
         <Route path='/register' element={<UserRegistration />} />
       </Routes>

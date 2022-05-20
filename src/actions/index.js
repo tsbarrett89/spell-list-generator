e3a37478ac57de8a91329687fed8582898f26d43
tsbarrett89 <tsbarrett89@gmail.com>
2022-05-20 14:39:49 -0600

    fetchSpells action finished

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,3 +4,15 @@ export const FETCH_SPELLS_START = "FETCH_SPELLS_START"
 export const FETCH_SPELLS_SUCCESS = "FETCH_SPELLS_SUCCESS"
 export const FETCH_SPELLS_FAILURE = "FETCH_SPELLS_FAILURE"
 export const SAVE_SPELL = "SAVE_SPELL"
+
+export const fetchSpells = spells => dispatch => {
+    dispatch({ type: SET_SPELLS_START, payload: spells })
+    axios
+        .get('https://www.dnd5eapi.co/api/spells')
+        .then(res => {
+            dispatch({ type: SET_SPELLS_SUCCESS, payload: res.data.results })
+        })
+        .catch( err => {
+            dispatch({ type: SET_SPELLS_FAILURE, payload: `Failed with ${err.status} status code.`})
+        })
+}

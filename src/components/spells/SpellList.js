a02f76cab3218f2faebb142caafa06d38ac457c8
tsbarrett89 <tsbarrett89@gmail.com>
2022-05-20 14:52:25 -0600

    imports done and SpellList exported

---
+++ src/components/spells/SpellList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+const SpellList = () => {
+
+    return (
+        <div></div>
+    )
+}
+
+export default SpellList

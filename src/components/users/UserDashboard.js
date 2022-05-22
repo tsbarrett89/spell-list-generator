cf48251953439e6b0c2ced681c5cc424a9a20639
tsbarrett89 <tsbarrett89@gmail.com>
2022-05-22 14:54:00 -0600

    user object added from reducer

--- src/components/users/UserDashboard.js
+++ src/components/users/UserDashboard.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 const UserDashboard = () => {
+    const user = useSelector((state) => state.authReducer.user)
+
     return (
         <div></div>
     )

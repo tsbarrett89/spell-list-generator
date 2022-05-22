9dc6a64adb379f752f8487358f6cb72b22bbab7f
tsbarrett89 <tsbarrett89@gmail.com>
2022-05-22 00:23:31 -0600

    combined reducers

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,46 +1,11 @@
-import {
-    FETCH_SPELLS_START,
-    FETCH_SPELLS_SUCCESS,
-    FETCH_SPELLS_FAILURE,
-    SAVE_SPELL
-} from '../actions'
+import { combineReducers } from "redux";
 
-const initialState = {
-    spells: [],
-    savedSpells: [],
-    isFetching: false,
-    errorMessage: ''
-}
+import authReducer from './authReducer'
+import characterReducer from './characterReducer'
+import spellReducer from './spellReducer'
 
-const reducer = (state = initialState, action) => {
-    switch(action.type){
-        case FETCH_SPELLS_START:
-            return {
-                ...state,
-                isFetching: true
-            }
-        case FETCH_SPELLS_SUCCESS:
-            return {
-                ...state,
-                spells: action.payload,
-                isFetching: false
-            }
-        case FETCH_SPELLS_FAILURE:
-            return {
-                ...state,
-                isFetching: false,
-                errorMessage: action.payload
-            }
-        case SAVE_SPELL:
-            return {
-                ...state,
-                savedSpells: [
-                    ...state.savedSpells,
-                    action.payload
-                ]
-            }
-        default: return state
-    }
-}
-
-export default reducer
+export default combineReducers({
+    authReducer,
+    characterReducer,
+    spellReducer
+})

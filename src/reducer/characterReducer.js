b270bfc3034a062655a126a97bc457f2417a666e
tsbarrett89 <tsbarrett89@gmail.com>
2022-05-22 15:04:18 -0600

    character actions imported

--- src/reducer/characterReducer.js
+++ src/reducer/characterReducer.js
@@ -1,3 +1,12 @@
+import {
+    FETCH_CHARACTERS_START,
+    FETCH_CHARACTERS_SUCCESS,
+    FETCH_CHARACTERS_FAILURE,
+    CREATE_CHARACTER_START,
+    CREATE_CHARACTER_SUCCESS,
+    CREATE_CHARACTER_FAILURE
+} from '../actions/characterActions';
+
 const initialState = {
     characters: [],
     errorMessage: ""

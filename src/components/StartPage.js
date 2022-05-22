c1305148427b35b30cf5d5f7c42f6c9e93bce827
tsbarrett89 <tsbarrett89@gmail.com>
2022-05-22 13:19:16 -0600

    html structure for startPage

--- src/components/StartPage.js
+++ src/components/StartPage.js
@@ -1,8 +1,22 @@
 import React from 'react';
 
+import UserLogin from './users/UserLogin'
+
 const StartPage = () => {
     return (
-        <div></div>
+        <div>
+            <div>
+                <p>Log in to create spell lists for your Dungeons and Dragons character. Or search spells and set a quick spell list.</p>
+            </div>
+            <div>
+                <div>
+                    <button>
+                        Spells
+                    </button>
+                </div>
+                <UserLogin />
+            </div>
+        </div>
     )
 }
 

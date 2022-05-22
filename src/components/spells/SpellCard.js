820d2ed010484bc06b4d16a92b296a9c488bbdc6
tsbarrett89 <tsbarrett89@gmail.com>
2022-05-21 23:21:23 -0600

    fetching and showSpell state set and handleClick started

--- src/components/spells/SpellCard.js
+++ src/components/spells/SpellCard.js
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const SpellCard = (props) => {
+    const [ fetching, setFetching ] = useState(false)
+    const [ showSpell, setShowSpell ] = useState(false)
+
+    handleClick = () => {
+        setFetching(true)
+        useEffect(() => {
+            
+        }, [])
+    }
     
     return (
         <div>

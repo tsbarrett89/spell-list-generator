1c54da07fcb79f7c3dace17e05d7ee79a72110b1
tsbarrett89 <tsbarrett89@gmail.com>
2022-05-22 14:12:51 -0600

    getCharacters action created

--- src/actions/characterActions.js
+++ src/actions/characterActions.js
@@ -6,3 +6,15 @@ export const FETCH_CHARACTERS_FAILURE = "FETCH_CHARACTERS_FAILURE"
 export const CREATE_CHARACTER_START = "CREATE_CHARACTER_START"
 export const CREATE_CHARACTER_SUCCESS = "CREATE_CHARACTER_SUCCESS"
 export const CREATE_CHARACTER_FAILURE = "CREATE_CHARACTER_FAILURE"
+
+export const getCharacters = user => dispatch => {
+    dispatch({ type: FETCH_CHARACTERS_START })
+    axios
+        .get(`http://localhost:5000/character/${user}`)
+        .then(res => {
+            dispatch({ type: FETCH_CHARACTERS_SUCCESS, payload: res.data })
+        })
+        .catch(err => {
+            dispatch({ type: FETCH_CHARACTERS_FAILURE, payload: `Failed with ${err.status} status code.` })
+        })
+}

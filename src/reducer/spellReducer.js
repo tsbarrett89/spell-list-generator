56ab0152a6a508f88b59ff818a7573927ad2b252
tsbarrett89 <tsbarrett89@gmail.com>
2022-05-22 00:20:45 -0600

    separated reducers

---
+++ src/reducer/spellReducer.js
@@ -0,0 +1,46 @@
+import {
+    FETCH_SPELLS_START,
+    FETCH_SPELLS_SUCCESS,
+    FETCH_SPELLS_FAILURE,
+    SAVE_SPELL
+} from '../actions'
+
+const initialState = {
+    spells: [],
+    savedSpells: [],
+    isFetching: false,
+    errorMessage: ''
+}
+
+const reducer = (state = initialState, action) => {
+    switch(action.type){
+        case FETCH_SPELLS_START:
+            return {
+                ...state,
+                isFetching: true
+            }
+        case FETCH_SPELLS_SUCCESS:
+            return {
+                ...state,
+                spells: action.payload,
+                isFetching: false
+            }
+        case FETCH_SPELLS_FAILURE:
+            return {
+                ...state,
+                isFetching: false,
+                errorMessage: action.payload
+            }
+        case SAVE_SPELL:
+            return {
+                ...state,
+                savedSpells: [
+                    ...state.savedSpells,
+                    action.payload
+                ]
+            }
+        default: return state
+    }
+}
+
+export default reducer

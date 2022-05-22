053b7827e1159d7eabe30f5112bb65aa3a71023c
tsbarrett89 <tsbarrett89@gmail.com>
2022-05-22 13:31:20 -0600

    savedSpells and dispatch assigned

--- src/components/spells/SavedList.js
+++ src/components/spells/SavedList.js
@@ -4,8 +4,13 @@ import { useSelector, useDispatch } from 'react-redux';
 import SpellCard from './SpellCard';
 
 const SavedList = () => {
+    const savedSpells = useSelector((state) => state.spellReducer.savedSpells)
+    const dispatch = useDispatch()
+    
     return (
-        <div></div>
+        <div>
+
+        </div>
     )
 }
 

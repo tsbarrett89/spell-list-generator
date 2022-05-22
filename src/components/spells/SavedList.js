817bc6b7ccfece16d5606291a8c336db6ab2a181
tsbarrett89 <tsbarrett89@gmail.com>
2022-05-22 13:28:08 -0600

    SavedList imports and export set

---
+++ src/components/spells/SavedList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import SpellCard from './SpellCard';
+
+const SavedList = () => {
+    return (
+        <div></div>
+    )
+}
+
+export default SavedList

e9bcad8a5708f80c5dff71e58a9da5c56f032cc4
tsbarrett89 <tsbarrett89@gmail.com>
2022-05-23 11:50:40 -0600

    dispatch getCharacters in useEffect

--- src/components/characters/CharacterList.js
+++ src/components/characters/CharacterList.js
@@ -1,10 +1,18 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { getCharacters } from '../../actions/characterActions'
 
 import CharacterCard from './CharacterCard';
 
 const CharacterList = () => {
+    const user = useSelector((state) => state.authReducer.user)
+    const dispatch = useDispatch()
     const characters = useSelector((state) => state.characterReducer.characters)
+
+    useEffect(() => {
+        dispatch(getCharacters(user.user_id))
+    }, [dispatch])
 
     return (
         <div>

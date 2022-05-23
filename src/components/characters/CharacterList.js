fd5518b4ce0a992aab031f4b56efaecf538184a2
tsbarrett89 <tsbarrett89@gmail.com>
2022-05-23 10:16:56 -0600

    get characters from reducer, map over characters and passes props to CharacterCard

--- src/components/characters/CharacterList.js
+++ src/components/characters/CharacterList.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+
+import CharacterCard from './CharacterCard';
 
 const CharacterList = () => {
+    const characters = useSelector((state) => state.characterReducer.characters)
+
     return (
-        <div></div>
+        <div>
+            {characters.map((character, index) => {
+                <CharacterCard character={character} key={index} />
+            })}
+        </div>
     )
 }
 
